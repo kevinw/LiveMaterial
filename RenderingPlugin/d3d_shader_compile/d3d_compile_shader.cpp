7ef94a84d1a35814b1d2b9a7ff356b249a89f8fa
Kevin Watters <kevinwatters@gmail.com>
2016-08-07 23:14:21 -0700

    beginning of reflection

--- RenderingPlugin/d3d_shader_compile/d3d_compile_shader.cpp
+++ RenderingPlugin/d3d_shader_compile/d3d_compile_shader.cpp
@@ -1,53 +1,142 @@
+// http://xboxforums.create.msdn.com/forums/t/32885.aspx
+#define INITGUID // todo: what to link against to get IID_ID3D11ShaderReflection?
+#include <d3d11shader.h>
+
+#include <d3d11.h>
 #include <d3dcompiler.h>
 #pragma comment(lib, "d3dcompiler.lib")
 
 #include <memory>
 #include <tchar.h>
-/*
-#include <ppltasks.h>
-#include <d3dcompiler.h>
-#include <Robuffer.h>
 
-void test()
+#include <vector>
+
+std::vector<D3D11_INPUT_ELEMENT_DESC> inputLayoutArray;
+ID3D11InputLayout* vertexLayout = nullptr;
+
+HRESULT enumInputLayout(ID3D11Device * d3dDevice, ID3DBlob * VSBlob)
 {
-	std::shared_ptr<Microsoft::WRL::ComPtr<ID3DBlob>> blobRef = std::make_shared<Microsoft::WRL::ComPtr<ID3DBlob>>();
+	HRESULT hr = S_OK;
 
-	// Load a file and compile it.
-	auto fileOp = Windows::ApplicationModel::Package::Current->InstalledLocation->GetFileAsync(L"shader.hlsl");
-	create_task(fileOp).then([this](Windows::Storage::StorageFile^ file) -> IAsyncOperation<Windows::Storage::Streams::IBuffer^>^
+	// Get description from precompiled shader
+	ID3D11ShaderReflection* vertReflect = nullptr;
+	D3DReflect(
+		VSBlob->GetBufferPointer(),
+		VSBlob->GetBufferSize(),
+		IID_ID3D11ShaderReflection,
+		(void**)&vertReflect
+	);
+
+	D3D11_SHADER_DESC descVertex;
+	vertReflect->GetDesc(&descVertex);
+
+	// save description of input parameters (attributes of vertex shader)
+	inputLayoutArray.clear();
+	uint32_t byteOffset = 0;
+	D3D11_SIGNATURE_PARAMETER_DESC input_desc;
+	for (size_t i = 0; i < descVertex.InputParameters; ++i) {
+		// get description of input parameter
+		vertReflect->GetInputParameterDesc(i, &input_desc);
+
+		// fill element description to create input layout later
+		D3D11_INPUT_ELEMENT_DESC ie;
+		ie.SemanticName = input_desc.SemanticName;
+		ie.SemanticIndex = input_desc.SemanticIndex;
+		ie.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+		ie.InputSlot = i;
+		ie.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+		ie.InstanceDataStepRate = 0;
+	ie.AlignedByteOffset = byteOffset;
+
+	// determine correct format of input parameter and offset
+	if (input_desc.Mask == 1)
 	{
-		// Do file I/O in background thread (use_arbitrary).
-		return Windows::Storage::FileIO::ReadBufferAsync(file);
-	}, task_continuation_context::use_arbitrary())
-		.then([this, blobRef](Windows::Storage::Streams::IBuffer^ buffer)
+		if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_UINT32)
+		{
+			ie.Format = DXGI_FORMAT_R32_UINT;
+		}
+		else if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_SINT32)
+		{
+			ie.Format = DXGI_FORMAT_R32_SINT;
+		}
+		else if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32)
+		{
+			ie.Format = DXGI_FORMAT_R32_FLOAT;
+		}
+		byteOffset += 4;
+	}
+	else if (input_desc.Mask <= 3)
 	{
-		// Do compilation in background thread (use_arbitrary).
-
-		// Cast to Object^, then to its underlying IInspectable interface.
-		Microsoft::WRL::ComPtr<IInspectable> insp(reinterpret_cast<IInspectable*>(buffer));
-
-		// Query the IBufferByteAccess interface.
-		Microsoft::WRL::ComPtr<Windows::Storage::Streams::IBufferByteAccess> bufferByteAccess;
-		insp.As(&bufferByteAccess);
-
-		// Retrieve the buffer data.
-		byte *pBytes = nullptr;
-		bufferByteAccess->Buffer(&pBytes);
-
-		Microsoft::WRL::ComPtr<ID3DBlob> blob;
-		Microsoft::WRL::ComPtr<ID3DBlob> errMsgs;
-		D3DCompile2(pBytes, buffer->Length, "shader.hlsl", nullptr, nullptr, "main", "ps_5_0", 0, 0, 0, nullptr, 0, blob.GetAddressOf(), errMsgs.GetAddressOf());
-		*blobRef = blob;
-	}, task_continuation_context::use_arbitrary())
-		.then([this, blobRef]()
+		if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_UINT32)
+		{
+			ie.Format = DXGI_FORMAT_R32G32_UINT;
+		}
+		else if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_SINT32)
+		{
+			ie.Format = DXGI_FORMAT_R32G32_SINT;
+		}
+		else if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32)
+		{
+			ie.Format = DXGI_FORMAT_R32G32_FLOAT;
+		}
+		byteOffset += 8;
+	}
+	else if (input_desc.Mask <= 7)
 	{
-		// Update UI / use shader on foreground thread.
-		wchar_t message[40];
-		swprintf_s(message, L"blob is %u bytes long", (unsigned)(*blobRef)->GetBufferSize());
-		this->TheButton->Content = ref new Platform::String(message);
-	}, task_continuation_context::use_current());
+		if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_UINT32)
+		{
+			ie.Format = DXGI_FORMAT_R32G32B32_UINT;
+		}
+		else if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_SINT32)
+		{
+			ie.Format = DXGI_FORMAT_R32G32B32_SINT;
+		}
+		else if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32)
+		{
+			ie.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+		}
+		byteOffset += 12;
+	}
+	else if (input_desc.Mask <= 15)
+	{
+		if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_UINT32)
+		{
+			ie.Format = DXGI_FORMAT_R32G32B32A32_UINT;
+		}
+		else if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_SINT32)
+		{
+			ie.Format = DXGI_FORMAT_R32G32B32A32_SINT;
+		}
+		else if (input_desc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32)
+		{
+			ie.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+		}
+		byteOffset += 16;
+	}
+
+	inputLayoutArray.push_back(ie);
+
+	// you can save input_desc here (if needed)
+	}
+
+		// create input layout from previosly created description
+	size_t numElements = inputLayoutArray.size();
+	hr = d3dDevice->CreateInputLayout(
+		inputLayoutArray.data(),
+		numElements,
+		VSBlob->GetBufferPointer(),
+		VSBlob->GetBufferSize(),
+		&vertexLayout
+	);
+
+	if (FAILED(hr))
+	{
+		// impossible to create input layout
+		return hr;
+	}
+
+	return S_OK;
 }
-*/
 
 
 HRESULT CompileShader(_In_ const char* src, _In_ LPCSTR srcName, _In_ LPCSTR entryPoint, _In_ LPCSTR profile, const D3D_SHADER_MACRO defines[], _Outptr_ ID3DBlob** blob, _Outptr_ ID3DBlob** errorBlob)
