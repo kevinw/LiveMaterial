c9f8db428e8d40f97ca764fe3718dd8db478f6fa
Kevin Watters <kevinwatters@gmail.com>
2016-10-16 21:57:19 -0700

    remove unused code

--- NativeRenderingPlugin/PluginSource/source/RenderingPlugin.cpp
+++ NativeRenderingPlugin/PluginSource/source/RenderingPlugin.cpp
@@ -17,11 +17,6 @@ string GetShaderIncludePath() { return s_shaderIncludePath; }
 
 
 // --------------------------------------------------------------------------
-// SetTimeFromUnity, an example function we export which is called by one of the scripts.
-
-static float g_Time = 0;
-
-// --------------------------------------------------------------------------
 // SetTextureFromUnity, an example function we export which is called by one of the scripts.
 
 static void* g_TextureHandle = NULL;
@@ -177,81 +172,6 @@ extern "C" {
 
 mutex renderAPIMutex;
 RenderAPI* GetCurrentRenderAPI() { return s_CurrentAPI; }
-
-static void DrawColoredTriangle(int uniformIndex) {
-	// Draw a colored triangle. Note that colors will come out differently
-	// in D3D and OpenGL, for example, since they expect color bytes
-	// in different ordering.
-	struct MyVertex {
-		float x, y, z;
-		unsigned int color;
-	};
-	MyVertex verts[3] = {
-		{ -0.5f, -0.25f,  0, 0xFFff0000 },
-		{ 0.5f, -0.25f,  0, 0xFF00ff00 },
-		{ 0,     0.5f ,  0, 0xFF0000ff },
-	};
-
-	// Transformation matrix: rotate around Z axis based on time.
-	float phi = g_Time; // time set externally from Unity script
-	float cosPhi = cosf(phi);
-	float sinPhi = sinf(phi);
-	float worldMatrix[16] = {
-		cosPhi,-sinPhi,0,0,
-		sinPhi,cosPhi,0,0,
-		0,0,1,0,
-		0,0,0.7f,1,
-	};
-
-	s_CurrentAPI->DrawSimpleTriangles(worldMatrix, 1, verts);
-}
-
-
-static void ModifyTexturePixels() {
-	void* textureHandle = g_TextureHandle;
-	int width = g_TextureWidth;
-	int height = g_TextureHeight;
-	if (!textureHandle)
-		return;
-
-	int textureRowPitch;
-	void* textureDataPtr = (unsigned char*)s_CurrentAPI->BeginModifyTexture(textureHandle, width, height, &textureRowPitch);
-	if (!textureDataPtr)
-		return;
-
-	const float t = g_Time * 4.0f;
-
-	unsigned char* dst = (unsigned char*)textureDataPtr;
-	for (int y = 0; y < height; ++y)
-	{
-		unsigned char* ptr = dst;
-		for (int x = 0; x < width; ++x)
-		{
-			// Simple "plasma effect": several combined sine waves
-			int vv = int(
-				(127.0f + (127.0f * sinf(x / 7.0f + t))) +
-				(127.0f + (127.0f * sinf(y / 5.0f - t))) +
-				(127.0f + (127.0f * sinf((x + y) / 6.0f - t))) +
-				(127.0f + (127.0f * sinf(sqrtf(float(x*x + y*y)) / 4.0f - t)))
-				) / 4;
-
-			// Write the texture pixel
-			ptr[0] = vv;
-			ptr[1] = vv;
-			ptr[2] = vv;
-			ptr[3] = vv;
-
-			// To next pixel (our pixels are 4 bpp)
-			ptr += 4;
-		}
-
-		// To next image row
-		dst += textureRowPitch;
-	}
-
-	s_CurrentAPI->EndModifyTexture(textureHandle, width, height, textureRowPitch, textureDataPtr);
-}
-
 
 static void UNITY_INTERFACE_API OnRenderEvent(int packedValue) {
 	if (s_CurrentAPI == nullptr)
